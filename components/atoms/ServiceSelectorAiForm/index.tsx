35d99dd74ce23332cd4fcdebdbacb31d9f8a485d
Muhammad Indrawan (Indrawan) <indrawanismail0@gmail.com>
2026-05-09 20:16:57 +0800

    add responsive

--- components/atoms/ServiceSelectorAiForm/index.tsx
+++ components/atoms/ServiceSelectorAiForm/index.tsx
@@ -41,26 +41,26 @@ export default function ServiceSelectorAiForm() {
                     {/* kepala */}
                     {areas.map(area => (
                         <div
-                            className="w-62 h-28"
+                            className="w-32 md:w-62 h-22 md:h-28"
                             key={area.id}>
                             <button
                                 value='kepala'
                                 onClick={() => handleSelectedBtn(area.id)}
                                 className={
-                                    `border rounded-xl cursor-pointer w-full h-full p-4 transition duration-300 ${selected.includes(area.id) ? 'border-4 border-[#8B6B52]' : ''}`
+                                    `border rounded-xl cursor-pointer w-full h-full p-2 md:p-4 transition duration-300 ${selected.includes(area.id) ? 'border-4 border-[#8B6B52]' : ''}`
                                 }
                                 type="button"
 
                             >
-                                <h2 className="text-left mb-2">{area.label}</h2>
-                                <p className="text-left text-gray-400">{area.desc}</p>
+                                <h2 className="text-center md:text-left mb-2">{area.label}</h2>
+                                <p className="text-center md:text-left text-xs text-gray-400">{area.desc}</p>
                             </button>
                         </div>
                     ))}
                 </section>
                 {/* area gambar tubuh start*/}
                 <section className="mt-6 flex justify-center items-center">
-                    <div className="w-42 h-120">
+                    <div className="w-28 h-72 md:w-42 md:h-120">
                         <Image
                             src={getSelectedImage(selected, abu)}
                             alt='test'
