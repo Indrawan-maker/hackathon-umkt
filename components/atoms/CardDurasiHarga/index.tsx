013a835601c38ab15a898970c5ea3726f4c12402
Muhammad Indrawan (Indrawan) <indrawanismail0@gmail.com>
2026-05-10 22:33:28 +0800

    fix: ring kedean

--- components/atoms/CardDurasiHarga/index.tsx
+++ components/atoms/CardDurasiHarga/index.tsx
@@ -11,7 +11,7 @@ export default function CardDurasiHarga({
     return (
         <>
         <section className="">
-            <div className="border-blue-900 border-2 rounded-2xl w-72 h-62 cursor-pointer">
+            <div className="border-blue-900 border-2 rounded-2xl w-full max-w-72 min-h-62 cursor-pointer">
             <div className="bg-blue-500 rounded-t-2xl flex justify-center items-center h-16 px-2 py-2 gap-6">
                 <div className="border-2 border-blue-300 rounded-full w-8 h-8 flex justify-center items-center">
                 <IoSparklesOutline className="w-4 h-4 text-white"/>
